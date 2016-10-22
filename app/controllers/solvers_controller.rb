2f3352f7ac1c71c80c99f2677e14488f3664099a
Yuya.Nishida <yuya@j96.org>
2016-10-22 19:45:51 +0900

    インデントの修正

--- app/controllers/solvers_controller.rb
+++ app/controllers/solvers_controller.rb
@@ -62,13 +62,14 @@ class SolversController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_solver
-      @solver = Solver.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def solver_params
-      params.require(:solver).permit(:username, :email, :content, :elapsed_usec, :nbytes)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_solver
+    @solver = Solver.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def solver_params
+    params.require(:solver).permit(:username, :email, :content, :elapsed_usec, :nbytes)
+  end
 end
