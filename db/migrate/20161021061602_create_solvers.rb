cfebb4f10edc5d27b4234ddbea2f8c048498da97
Yuya.Nishida <yuya@j96.org>
2016-10-22 15:18:41 +0900

    NOT NULL制約を付与

--- db/migrate/20161021061602_create_solvers.rb
+++ db/migrate/20161021061602_create_solvers.rb
@@ -1,11 +1,11 @@
 class CreateSolvers < ActiveRecord::Migration[5.0]
   def change
     create_table :solvers do |t|
-      t.string :username
-      t.string :email
-      t.text :content
-      t.integer :elapsed_usec
-      t.integer :nbytes
+      t.string :username, null: false
+      t.string :email, null: false
+      t.text :content, null: false
+      t.integer :elapsed_usec, null: false
+      t.integer :nbytes, null: false
 
       t.timestamps
     end
