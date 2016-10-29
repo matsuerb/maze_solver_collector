61bc65d7a3e7afa57c9e9993c1556cee9f81ca24
Yuya.Nishida <yuya@j96.org>
2016-10-29 12:08:16 +0900

    Maze#questionを追加

--- app/models/maze.rb
+++ app/models/maze.rb
@@ -1,3 +1,7 @@
 class Maze < ApplicationRecord
   validates :correct_answer, uniqueness: true
+
+  def question
+    return correct_answer.gsub(":", " ")
+  end
 end
--- test/factories/mazes.rb
+++ test/factories/mazes.rb
@@ -1,5 +1,17 @@
 FactoryGirl.define do
-  factory :maze do
-    correct_answer "MyText"
+  factory(:maze) do
+    correct_answer(<<EOS)
+###########
+S:#:::::# #
+#:#:###:# #
+#:::#:::# #
+#####:### #
+#:::::#   #
+#:### # ###
+#:::# # # #
+###:### # #
+#  :::::::G
+###########
+EOS
   end
 end
