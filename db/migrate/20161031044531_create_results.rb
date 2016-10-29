46695d343ecf0aed88d59af2d540cce376218b40
Yuya.Nishida <yuya@j96.org>
2016-10-29 13:53:36 +0900

    NOT NULL制約やインデックスの付与

--- db/migrate/20161031044531_create_results.rb
+++ db/migrate/20161031044531_create_results.rb
@@ -1,11 +1,12 @@
 class CreateResults < ActiveRecord::Migration[5.0]
   def change
     create_table :results do |t|
-      t.references :maze, foreign_key: true
-      t.references :solver, foreign_key: true
-      t.integer :elapsed_usec
+      t.references :maze, foreign_key: true, index: false, null: false
+      t.references :solver, foreign_key: true, index: false, null: false
+      t.integer :elapsed_usec, null: false
 
       t.timestamps
+      t.index [:solver_id, :maze_id], unique: true
     end
   end
 end
