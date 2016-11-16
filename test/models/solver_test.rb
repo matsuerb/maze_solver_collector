ac4b21d69c420d24ff3577bf6dfa4ab86a158d55
Yuya.Nishida <yuya@j96.org>
2016-11-16 21:48:23 +0900

    リファクタリング

--- test/models/solver_test.rb
+++ test/models/solver_test.rb
@@ -20,44 +20,32 @@ class SolverTest < ActiveSupport::TestCase
       assert_equal(-1, solver.elapsed_usec)
     end
 
-    test("Kernel#sleepは効かない") do
-      solver = build(:valid_solver, content: <<EOS)
-sleep(10)
-EOS
-      start_time = Time.now
-      solver.run_and_set_result
-      end_time = Time.now
-      assert_operator(10, :>, end_time - start_time)
+    def self.omit_feature_test(name, code_template)
+      test_sec = 10
+      code = code_template.gsub("%{sec}", test_sec.to_s)
+      test(name) do
+        solver = build(:valid_solver, content: code)
+        start_time = Time.now
+        solver.run_and_set_result
+        end_time = Time.now
+        assert_operator(test_sec, :>, end_time - start_time)
+      end
     end
 
-    test("/bin/sleepは効かない") do
-      solver = build(:valid_solver, content: <<EOS)
-system("sleep 10")
+    omit_feature_test("Kernel#sleepは効かない", <<EOS)
+sleep(%{sec})
 EOS
-      start_time = Time.now
-      solver.run_and_set_result
-      end_time = Time.now
-      assert_operator(10, :>, end_time - start_time)
-    end
 
-    test("Perlは実行できない") do
-      solver = build(:valid_solver, content: <<EOS)
-system("perl", "-e", "sleep(10);")
+    omit_feature_test("/bin/sleepは効かない", <<EOS)
+system("sleep %{sec}")
 EOS
-      start_time = Time.now
-      solver.run_and_set_result
-      end_time = Time.now
-      assert_operator(10, :>, end_time - start_time)
-    end
 
-    test("Pythonは実行できない") do
-      solver = build(:valid_solver, content: <<EOS)
-system("python", "-c", "import time; time.sleep(10);")
+    omit_feature_test("Perlは実行できない", <<EOS)
+system("perl", "-e", "sleep(%{sec});")
 EOS
-      start_time = Time.now
-      solver.run_and_set_result
-      end_time = Time.now
-      assert_operator(10, :>, end_time - start_time)
-    end
+
+    omit_feature_test("Pythonは実行できない", <<EOS)
+system("python", "-c", "import time; time.sleep(%{sec});")
+EOS
   end
 end
