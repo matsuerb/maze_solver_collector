29f7ff94fe5edeb1c3f3e406df2ff3bbb4ddebcd
Yuya.Nishida <yuya@j96.org>
2016-11-16 20:25:45 +0900

    Result#elapsed_secの追加

--- app/models/result.rb
+++ app/models/result.rb
@@ -5,4 +5,8 @@ class Result < ApplicationRecord
   def success?
     return elapsed_usec >= 0
   end
+
+  def elapsed_sec
+    return elapsed_usec / 1_000_000.0
+  end
 end
--- test/models/result_test.rb
+++ test/models/result_test.rb
@@ -16,4 +16,11 @@ class ResultTest < ActiveSupport::TestCase
       assert_equal([false], solver.results.map(&:success?))
     end
   end
+
+  sub_test_case("#elapsed_sec") do
+    test("処理時間を秒数で返す") do
+      result = Result.new(elapsed_usec: 1)
+      assert_equal(0.000_001, result.elapsed_sec)
+    end
+  end
 end
