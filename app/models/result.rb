2a9895ddde62ded5c94622deeb4aee51765bd02c
Yuya.Nishida <yuya@j96.org>
2016-11-16 20:25:27 +0900

    Result#success?の追加

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,8 @@
 class Result < ApplicationRecord
   belongs_to :maze
   belongs_to :solver
+
+  def success?
+    return elapsed_usec >= 0
+  end
 end
--- test/models/result_test.rb
+++ test/models/result_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ResultTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  sub_test_case("#success?") do
+    setup do
+      create(:maze)
+    end
+
+    test("成功時はtrue") do
+      solver = create(:valid_solver)
+      assert_equal([true], solver.results.map(&:success?))
+    end
+
+    test("失敗時はfalse") do
+      solver = create(:invalid_solver)
+      assert_equal([false], solver.results.map(&:success?))
+    end
+  end
 end
