b1b1227b3c72e89929bacf7e4dc9c285689c9d6d
Toru Kurahashi <toru_kurahashi@eastback.jp>
2016-11-17 13:34:03 +0900

    before_actionを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :basic if Rails.env.production?
+  before_action :basic if: -> { Rails.env.production? }
 
   private
 
