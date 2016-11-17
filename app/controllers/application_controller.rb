bc025a9a16752696868a295ee48d4d6f6a1f4c4b
Toru Kurahashi <toru_kurahashi@eastback.jp>
2016-11-17 11:40:01 +0900

    Basic認証の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :basic
+
+  private
+
+  def basic
+    authenticate_or_request_with_http_basic do |user, password|
+      user == 'matsuerb' && password == 'procon'
+    end
+  end
 end
