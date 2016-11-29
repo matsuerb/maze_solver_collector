07f0bf93948fa2b9943402c0ba24947c3d358255
tm21 <yoshioka@tm-21.com>
2016-11-29 13:11:55 +0900

    migrationファイル内のclassの定義位置の変更

--- db/migrate/20161128121447_add_division_to_solvers.rb
+++ db/migrate/20161128121447_add_division_to_solvers.rb
@@ -1,12 +1,12 @@
-# 将来Solverモデルが消えてる時にこのmigrationを通ってもSolverモデルがなくて例外にならなくするために定義。
-class Solver < ApplicationRecord
-  DivisionVal={
-      general: 0, # 一般
-      student: 1, # 学生
-  }
-end
-
 class AddDivisionToSolvers < ActiveRecord::Migration[5.0]
+  # 将来Solverモデルが消えてる時にこのmigrationを通ってもSolverモデルがなくて例外にならなくするために定義。
+  class Solver < ApplicationRecord
+    DivisionVal={
+        general: 0, # 一般
+        student: 1, # 学生
+    }
+  end
+
   def up
     add_column :solvers, :division, :integer, default: 0, null: false
     begin
